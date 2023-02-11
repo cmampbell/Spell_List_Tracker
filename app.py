cebeb775684b46043e77a84b79e16877d770b24a
Matt Campbell <cmampbell@gmail.com>
2023-02-11 11:58:44 -0500

    Finished tests for signup/login/logout

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sqlalchemy.exc import IntegrityError
 from models import db, connect_db, User
 from forms import UserSignUpForm, UserLoginForm
 
+
 CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
@@ -18,9 +19,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
@@ -32,7 +33,7 @@ def add_user_to_g():
     """If we're logged in, add curr user to Flask global."""
 
     if CURR_USER_KEY in session:
-        g.user = User.query.get(session[CURR_USER_KEY])
+        g.user = db.session.get(User, session[CURR_USER_KEY])
 
     else:
         g.user = None
@@ -68,7 +69,7 @@ def signup():
             user = User.signup(
                 username=form.username.data,
                 password=form.password.data,
-                email=form.email.data,
+                email=form.email.data
             )
             db.session.commit()
 
--- test_user_views.py
+++ test_user_views.py
@@ -1,19 +1,25 @@
 '''User view function tests'''
+#test with python -m unittest test_user_views.py
 
 import os
 from unittest import TestCase
 from flask import session
 
-from models import db, connect_db, User
+from models import db, User
 
 os.environ['DATABASE_URL'] = 'postgresql:///spell-tracker-test'
 
 from app import app, CURR_USER_KEY
 
+app.app_context().push()
+
 db.create_all()
 
 #disable WTForms csrf for testing
 app.config['WTF_CSRF_ENABLED'] = False
+app.config['TESTING'] = True
+
+test_password = 'HASHED_PASSWORD'
 
 class UserViewTestCase(TestCase):
     '''Test views for users'''
@@ -23,22 +29,76 @@ class UserViewTestCase(TestCase):
         User.query.delete()
 
         self.client = app.test_client()
+        user = User.signup(
+            username='test_user',
+            password= test_password,
+            email='test@hotmail.com'
+        )
+        db.session.commit()
 
+        self.user = user
+
+    def tearDown(self) -> None:
+        db.session.rollback()
+    
     def test_user_signup(self):
         '''Does the sign-up route work as intended?'''
         with self.client as client:
-            d = {
-                'username': 'testuser', 
-                'email':'test@hotmail.com', 
-                'password': 'HASHED_PASSWORD',
-                'confirm': 'HASHED_PASSWORD'}
-            resp = client.post(f"/signup", data=d, follow_redirects=True)
+            data = {
+                'username': 'testuser2', 
+                'email':'test2@hotmail.com', 
+                'password': test_password,
+                'confirm': 'HASHED_PASSWORD'
+                }
+                
+            resp = client.post("/signup", data=data)
 
-            #check that status code is ok
-            self.assertEqual(resp.status_code, 200)
+            # check that status code is redirect
+            self.assertEqual(resp.status_code, 302)
 
-            #check that user has been added to database and session
-            self.assertIsNotNone(session.get('curr_user'))
-            self.assertEqual(len(User.query.all()), 1)
+            # check that user has been added to database and session
+            self.assertIsNotNone(session.get(CURR_USER_KEY))
+            self.assertEqual(len(User.query.all()), 2)
+
+            testuser = db.session.get(User, session[CURR_USER_KEY])
+
+            self.assertEqual(testuser.username, data['username'])
+
+    def test_user_login(self):
+        '''Test user login method'''
+
+        with self.client as client:
+            data={
+                'username': self.user.username,
+                'password': test_password
+                }
+            resp = client.post('/login', data=data)
+
+            #check status code is redirect
+            self.assertEqual(resp.status_code, 302)
+
+            #check that current user in session matched our user id
+            self.assertEqual(session.get(CURR_USER_KEY), self.user.id)
+
+    def test_user_logout(self):
+        '''Test user logout method'''
+
+        with self.client as client:
+            # do login
+            # session_transaction seemed to work on a seperate session
+            # do_logout() did not see session_transaction session
+            data={
+                'username': self.user.username,
+                'password': test_password
+                }
+            client.post('/login', data=data)
+        
+            resp = client.get('/logout')
+
+            #check that we get ok status code
+            self.assertEqual(resp.status_code, 302)
+
+            #check that current user is removed from session
+            self.assertIsNone(session.get(CURR_USER_KEY))
 
 
