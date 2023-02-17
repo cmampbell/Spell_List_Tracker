b7342d18f6843d9c7a770f2fd7bc33a60f44b85e
Matt Campbell <cmampbell@gmail.com>
2023-02-17 18:32:01 -0500

    Users can drag spells back to the available spell list to remove them from the list

--- static/spell_list.js
+++ static/spell_list.js
@@ -1,5 +1,11 @@
+//areas to drag and drop spells
 const dropZone = document.getElementById("target")
+const spellContainer = document.getElementById("spell-cards-div")
+
+// spell options in the hidden multiple select field
 const spellOptions = document.querySelectorAll("option")
+// select field for form
+const formSelectField = document.getElementById("spells");
 
 function allowDrop(evt) {
     evt.preventDefault();
@@ -9,7 +15,7 @@ function drag(evt) {
     evt.dataTransfer.setData("text", evt.target.id);
 }
 
-function drop(evt) {
+function addToListOnDrop(evt) {
     evt.preventDefault();
     let data = evt.dataTransfer.getData("text");
 
@@ -18,36 +24,30 @@ function drop(evt) {
     // set the spell in the hidden select field to true
     addedSpell.selected = true;
 
-    if(dropZone.classList.contains('empty-list')){
-        dropZone.classList.toggle('empty-list');
-        dropZone.classList.toggle('has-items');
-    }
-
     dropZone.appendChild(document.getElementById(data));
+
+    toggleDropZone()
 }
 
-function start() {
-    // hide the select field with all spell options
-    const formSelectField = document.getElementById("spells");
-    formSelectField.hidden = true;
+function returnToSpellsOnDrop(evt) {
+    evt.preventDefault();
+    let data = evt.dataTransfer.getData("text");
 
-    // get all spell cards
-    const allSpellCards = document.getElementById("avail-spells");
+    // find the spell that was added to the list
+    let addedSpell = Array.from(spellOptions).find(spell => spell.value == data)
+    // set the spell in the hidden select field to true
+    addedSpell.selected = false;
 
-    const spellCards = allSpellCards.children
+    spellContainer.appendChild(document.getElementById(data));
 
-    for (card of spellCards){
-        card.addEventListener("dragstart", drag);
-    }
+    //make an array of all spells
+    let spellArray = Array.from(spellContainer.children).sort(compare);
 
-    dropZone.addEventListener("dragover", allowDrop)
-    dropZone.addEventListener("drop", drop)
+    //append sorted list back to DOM
+    spellArray.forEach(elem => spellContainer.appendChild(elem));
 
-    const clearButton = document.getElementById('clear-button')
-    clearButton.addEventListener("click", clearSpellList)
+    toggleDropZone()
 }
-
-window.addEventListener("DOMContentLoaded", start)
 
 function compare(a, b) {
     if (parseInt(a.dataset.listPosition) < parseInt(b.dataset.listPosition))
@@ -59,11 +59,7 @@ function compare(a, b) {
   
 // Function to sort spells
 function clearSpellList() {
-    //reset drop-zone class
-    if(dropZone.classList.contains('has-items')){
-        dropZone.classList.toggle('empty-list')
-        dropZone.classList.toggle('has-items')
-    }
+
     // get all spells
     const allSpellCards = document.querySelectorAll("[data-list-position]");
 
@@ -75,22 +71,52 @@ function clearSpellList() {
 
     //clear options
     Array.from(spellOptions).forEach(elem => elem.selected = false)
+
+    //reset drop-zone class
+    toggleDropZone()
 }
 
+function toggleDropZone() {
+    if (dropZone.childElementCount === 0){
+        dropZone.classList.remove('has-items');
+        dropZone.classList.add('empty-list');
+    }
+    else if (dropZone.childElementCount > 0){
+        dropZone.classList.remove('empty-list');
+        dropZone.classList.add('has-items');
+    }
+}
+
+function start() {
+
+    // hide the select field with all spell options
+    formSelectField.hidden = true;
+
+    // get all spell cards
+
+    const spellCards = spellContainer.children
+
+    for (card of spellCards){
+        card.addEventListener("dragstart", drag);
+    }
+
+    dropZone.addEventListener("dragover", allowDrop)
+    dropZone.addEventListener("drop", addToListOnDrop)
+
+    spellContainer.addEventListener("dragover", allowDrop)
+    spellContainer.addEventListener("drop", returnToSpellsOnDrop)
+
+    const clearButton = document.getElementById('clear-button')
+    clearButton.addEventListener("click", clearSpellList)
+}
+
+window.addEventListener("DOMContentLoaded", start)
+
+
+
 // TODO:
-// Figure out how to save spells into a spell list on the server
-
-//      I think we hide a select field, and when a user drops a spell into the dropZone
-//         we add that selection into the hidden select field by spell id
-//         if a user clears the list, we clear the selections
-//          when a user hits save spell-list, we grab the selects from the hidden field and make the post request
-
-// Need to make spell selection hidden
-// When a spell is dropped in dropzone -> find spell id in select field, and make selected = True
-
 
 // Should refactor these functions into OOP
 // Should refactor server-side spell list functions into OOP, put them as methods in the
 //      character model
-// Set up page for users to look at their characters spell lists
 // Write tests!!!!!
