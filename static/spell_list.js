0c1933b9cba1a882b05236accb20ade74854bb6e
Matt Campbell <cmampbell@gmail.com>
2023-02-16 21:43:19 -0500

    Added buttons to clear and save list. Need to implement functionality

--- static/spell_list.js
+++ static/spell_list.js
@@ -13,7 +13,26 @@ function drop(evt) {
     let data = evt.dataTransfer.getData("text");
 
     let dropZone = document.getElementById("target")
+    if (dropZone.classList.contains('empty-list')){
+        dropZone.classList.toggle('empty-list')
+        dropZone.classList.toggle('has-items')
+    }
     dropZone.appendChild(document.getElementById(data));
+}
+
+function clickClearButton(evt){
+    console.log('in click')
+    evt.preventDefault()
+    const dropZone = document.getElementById("target")
+    const availSpells = document.getElementById('avail-spells')
+
+    for(child in dropZone.children){
+        console.log(child)
+        availSpells.prepend(child)
+    }
+    dropZone.innerHTML = ''
+    dropZone.classList.toggle('empty-list')
+    dropZone.classList.toggle('has-items')
 }
 
 function start() {
@@ -28,6 +47,22 @@ function start() {
     const dropZone = document.getElementById("target")
     dropZone.addEventListener("dragover", allowDrop)
     dropZone.addEventListener("drop", drop)
+
+    const clearButton = document.getElementById('clear-button')
+    clearButton.addEventListener("click", clickClearButton)
 }
 
 window.addEventListener("DOMContentLoaded", start)
+
+// TODO:
+// Get clear list button working
+//      on click it should clear the list of spells and reset their positions
+// Figure out how to save spells into a spell list on the server
+//      I'm thinking that we can get all the children from the DOM elements in target
+//      and grab their id's put them in a list, and using axios send that list to the server
+//      then we process the request server side, and save the spell list in the database
+// Should refactor these functions into OOP
+// Should refactor server-side spell list functions into OOP, put them as methods in the
+//      character model
+// Set up page for users to look at their characters spell lists
+// Write tests!!!!!
